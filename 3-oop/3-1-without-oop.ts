d8fe10d60fa581c87f526546ee6dad575dc30274
kimwooyoung <whoyoung90@naver.com>
2022-09-11 17:51:46 +0900

    study: 220911 class 공부 완료

--- 3-oop/3-1-without-oop.ts
+++ 3-oop/3-1-without-oop.ts
@@ -4,13 +4,14 @@
     hasMilk: boolean;
   };
 
-  const BEANS_GRAMM_PER_SHOT: number = 7;
+  const BEANS_GRAMM_PER_SHOT: number = 7; // 1샷에 7그램
   let coffeeBeans: number = 0; // 커피빈 그램수
 
   function makeCoffee(shots: number): CoffeeCup {
     if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
       throw new Error('Not enough coffee beans!');
-    }
+    };
+
     coffeeBeans -= shots * BEANS_GRAMM_PER_SHOT;
     return {
       shots, // key value 이름이 동일하면 생략 가능
