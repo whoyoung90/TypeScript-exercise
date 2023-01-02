794fac4243de0a37cdcf3bff0fa4de65c01c3a5d
kimwooyoung <whoyoung90@naver.com>
2023-01-02 23:35:24 +0900

    study: 230102 generic-project 공부 완료

--- 5-generic/5-3-0-constrains.ts
+++ 5-generic/5-3-0-constrains.ts
@@ -9,7 +9,6 @@
     }
     workFullTime() {}
   }
-
   class PartTimeEmployee implements Employee {
     pay() {
       console.log(`part time!!`);
@@ -17,14 +16,29 @@
     workPartTime() {}
   }
 
+  const ellie = new FullTimeEmployee();
+  const bob = new PartTimeEmployee();
+  ellie.pay();
+  bob.pay();
+  ellie.workFullTime();
+  bob.workPartTime();
+
   /**
    * ❌ 세부적인 타입을 인자로 받아서 "추상적인 타입으로 다시 리턴하는 함수는 💩💩💩"
    * 타입을 좀더 추상적으로 만들어야지, 타입을 광범위하게 만드는 행위는 위험!
+   *
+   * (Line 34) "다시 Employee만 리턴되기 때문에" 세부 클래스의 정보를 잃어버린다!
    */
   function payBad(employee: Employee): Employee {
     employee.pay();
     return employee;
   }
+  const ellieAfterPayBad = payBad(ellie);
+  const bobAfterPayBad = payBad(bob);
+  ellieAfterPayBad.pay();
+  bobAfterPayBad.pay();
+  // ellieAfterPayBad.workFullTime(); // ❌
+  // bobAfterPayBad.workPartTime(); // ❌
 
   /**
    * ✅ 세부적인 조건 constrains(1)
@@ -33,23 +47,14 @@
    *
    * Generic도 조건들을 걸어둠으로써 좀더 제한적인 범위 내에서 일반화된 Generic을 이용할 수 있다
    */
-  function pay<T extends Employee>(employee: T): T {
+  function payGood<T extends Employee>(employee: T): T {
     employee.pay(); // <T extends Employee>을 추가해야 .pay()가능
     return employee;
   }
-
-  const ellie = new FullTimeEmployee();
-  const bob = new PartTimeEmployee();
-  ellie.workFullTime();
-  bob.workPartTime();
-
-  // ❌ (Line24) "다시 Employee만 리턴되기 때문에" 세부 클래스의 정보를 잃어버린다!
-  const ellieAfterPayBad = payBad(ellie);
-  const bobAfterPayBad = payBad(bob);
-  // ellieAfterPayBad.workFullTime(); // 에러
-  // bobAfterPayBad.workPartTime(); // 에러
-
-  // ✅
-  const ellieAfterPay = pay(ellie);
-  const bobAfterPay = pay(bob);
+  const ellieAfterPay = payGood(ellie);
+  const bobAfterPay = payGood(bob);
+  ellieAfterPay.pay();
+  bobAfterPay.pay();
+  ellieAfterPay.workFullTime();
+  bobAfterPay.workPartTime();
 }
---
+++ 6-generic-project/stack.ts
@@ -0,0 +1,62 @@
+/* Generic을 이용할때는 interface, class, function 정의부분에서 <T>로 정의! */
+interface Stack<T> {
+  // Stack이라는 interface는 T라는 타입을 받는다
+  readonly size: number;
+  push(value: T): void;
+  pop(): T;
+}
+
+type StackNode<T> = {
+  // type StackNode는 <T>를 정의
+  readonly value: T;
+  readonly next?: StackNode<T>;
+};
+
+class StackImpl<T> implements Stack<T> {
+  private _size: number = 0;
+  private head?: StackNode<T>;
+
+  constructor(private capacity: number) {}
+  get size() {
+    return this._size;
+  }
+  push(value: T) {
+    if (this.size === this.capacity) {
+      throw new Error("Stack is full!");
+    }
+    const node = { value, next: this.head }; // head에 타입이 명시되어있기 때문에 타입 추론 활용! (명확한 부분에서는 타입 정보 생략)
+    this.head = node;
+    this._size++;
+  }
+  pop(): T {
+    if (this.head == null) {
+      throw new Error("Stack is empty!");
+    }
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.value;
+  }
+}
+
+/* 인스턴스를 생성할때 따로 타입을 명시하지 않으면 unknown 👉 <string> 정의 */
+const stack = new StackImpl<string>(10);
+stack.push("Ellie 1");
+stack.push("Bob 2");
+stack.push("Steve 3");
+while (stack.size !== 0) {
+  console.log(stack.pop());
+}
+
+const stack2 = new StackImpl<number>(10);
+stack2.push(123);
+stack2.push(456);
+stack2.push(789);
+while (stack2.size !== 0) {
+  console.log(stack2.pop());
+}
+
+/**
+ * 재사용성이 필요하면 Generic ✨
+ * 타입이 보장되면서 좀더 다양한 데이터를 담을 수 있다
+ */
