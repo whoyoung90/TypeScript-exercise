23ef3f087a9a2b83397fbf3f24f10cb6d488ce36
kimwooyoung <whoyoung90@naver.com>
2023-02-16 00:01:15 +0900

    study: 230215 디버깅 공부 완료

--- 11-config/src/logging/logging.ts
+++ 11-config/src/logging/logging.ts
@@ -2,11 +2,11 @@
  * @description 특정 타입스크립트 파일을 자바스크립트로 변경
  * @returns { tsc src/logging/logging.ts -w }
  *
- * @description tsconfig가 있는 폴더의 "모든" 타입스크립트를 자바스크립트로 변경
- * @returns { tsc -w }
- *
- * @description tsconfig.json 파일 생성 (여러 파일을 동시에 실행하고 좀더 다양한 옵션을 주기 위해)
+ * @description tsconfig.json 파일 생성 (여러 파일을 동시에 실행하고 다양한 옵션을 주기 위해)
  * @returns { tsc --init }
+ *
+ * @description tsconfig.json이 있는 프로젝트의 "모든" 타입스크립트를 자바스크립트로 변경
+ * @returns { tsc -w } tsconfig.json 설정대로 변경!
  */
 
 console.log("hello!");
--- 11-config/src/main.ts
+++ 11-config/src/main.ts
@@ -2,9 +2,9 @@
  * @description 컴파일된 js파일들의 저장 위치 지정
  * @returns { "outDir": "./build" }
  * src 폴더는 build안에 생성되지 않는다
- * => 프로젝트 제일 최상위 루트에서 "첫번쨰 타입스크립트 파일이 나오는 곳부터" 상위 폴더가 지정되기 때문!
+ * => 프로젝트 제일 최상위 루트에서 "첫번째 타입스크립트 파일이 나오는 곳부터" 상위 폴더가 지정되기 때문!
  *
- * @description src 디렉토리 밖에서는 절대 typescript파일을 생성할 수 없게 만드려면
+ * @description src 디렉토리 밖에서는 절대 typescript파일을 생성할 수 없도록
  * @returns { "rootDir": "./src" }
  *
  * @description 컴파일 되지 않아야 하는 파일 지정!
